c4ade525b0fdeeafa3d9b4f999fce080bc4d1021
Hansen Idden <hansenidden@gmail.com>
2022-10-22 00:13:04 +0700

    DONE product list model

--- flask-server/models/product_list.py
+++ flask-server/models/product_list.py
@@ -1,1 +1,12 @@
 from .base import db
+
+class ProductList(db.Model):
+    __tablename__ = "product_list"
+    details_id = db.Column(db.String(100), db.ForeignKey('product.id'), primary_key=True)
+    categories_id = db.Column(db.String(100), db.ForeignKey('categories.id'))
+
+    def __init__(self, id: str):
+        return self.details_id = id
+    
+    def __repr__(self):
+        return f"ProductList(item_id = {self.details_id}, categories = {self.categories_id})"
