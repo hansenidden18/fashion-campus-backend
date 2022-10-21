3b370c909019bbb041f40dc104acb68e595b6f0d
Hansen Idden <hansenidden@gmail.com>
2022-10-22 00:21:02 +0700

    Done categories model

--- flask-server/models/categories.py
+++ flask-server/models/categories.py
@@ -1,1 +1,13 @@
 from .base import db
+
+class Categories(db.Model):
+    __tablename__ = "categories"
+    
+    id = db.Column(db.String(100), primary_key = True, nullable=False)
+    title = db.Column(db.String(200))
+
+    def __init__(self, id:str):
+        return self.id == id
+    
+    def __repr__(self):
+        return f"Categories(id = {self.id}, title = {self.title})"
