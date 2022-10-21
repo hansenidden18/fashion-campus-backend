1233ab4e1c1632db9fac36a34db9bc75d0ed95cb
Hansen Idden <hansenidden@gmail.com>
2022-10-22 00:17:47 +0700

    FIX Product list model

--- flask-server/models/product_list.py
+++ flask-server/models/product_list.py
@@ -6,7 +6,7 @@ class ProductList(db.Model):
     categories_id = db.Column(db.String(100), db.ForeignKey('categories.id'))
 
     def __init__(self, id: str):
-        return self.details_id = id
+        return self.details_id == id
     
     def __repr__(self):
         return f"ProductList(item_id = {self.details_id}, categories = {self.categories_id})"
