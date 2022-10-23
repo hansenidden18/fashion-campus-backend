7dac2abf4a6c2fa5cd134e9397b1eafdcb29c59f
Muhammad Riandy <mriandy51@gmail.com>
2022-10-23 09:33:30 +0700

    "maaf telat, lupa di push euy. tolong koreksi yaa"

--- flask-server/models/cart.py
+++ flask-server/models/cart.py
@@ -1,14 +1,1 @@
 from .base import db
-from sqlalchemy.dialects.postgresql import ARRAY
-
-class Cart(db.model):
-    __tablename__ = "cart"
-    
-    id = db.Column(db.String(100), primary_key=True, nullable=False)
-    quantity = db.Column(db.Integer)
-    size = db.Column(ARRAY(db.String(100)))
-    price = db.Column(db.Integer)
-    image = db.Column(db.String(100))
-    name = db.Column(db.String(100), db.ForeignKey('product.id'))
-
-    
