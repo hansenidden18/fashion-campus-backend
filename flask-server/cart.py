f10c45cd8da6ca7fc12c15067c419b1dc687c55d
Hansen Idden <hansenidden@gmail.com>
2022-11-17 21:05:31 +0700

    Fix Cart

--- flask-server/cart.py
+++ flask-server/cart.py
@@ -1,3 +1,8 @@
-from flask import Blueprint
+from flask import Blueprint, request
 
 cart_bp = Blueprint("cart", __name__, url_prefix="")
+
+@cart_bp.route("/cart", methods=["GET"])
+def get_cart():
+
+    token = str(request.headers.get('token'))
